ae2235b732835e5e7119a4a86abc77db621e4f19
Gabecodex <gabrielfmdesouza@gmail.com>
2021-07-28 06:43:00 -0300

    Quary selector and click event

--- Scripts/Controller/CalcController.js
+++ Scripts/Controller/CalcController.js
@@ -8,6 +8,7 @@ class CalcController {
         this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
 
     }
 
@@ -20,6 +21,22 @@ class CalcController {
             this.setDisplayDateTime();
 
         }, 1000);
+
+    }
+
+    initButtonsEvents(){
+
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+        buttons.forEach((btn, index)=>{
+
+            btn.addEventListener('click', e => {
+
+                console.log(btn.className.baseVal.replace("btn-",""));
+
+            })
+
+        })
 
     }
 
@@ -81,5 +98,5 @@ class CalcController {
         this._currentDate = value;
 
     }
-    
+
 }
